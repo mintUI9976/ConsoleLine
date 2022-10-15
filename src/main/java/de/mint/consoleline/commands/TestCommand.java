cd37646849b694181e5a9cb0ed25177c75279531
mintUI9976 <niklas.griese4@gmail.com>
2022-10-15 15:14:52 +0200

    add TestCommand.java

---
+++ src/main/java/de/mint/consoleline/commands/TestCommand.java
@@ -0,0 +1,13 @@
+package de.mint.consoleline.commands;
+
+import de.mint.consoleline.commands.handler.CommandArguments;
+
+import java.util.List;
+
+public class TestCommand extends CommandArguments {
+
+  @Override
+  public void onCommand(final List<String> arguments) {
+    super.onCommand(arguments);
+  }
+}
