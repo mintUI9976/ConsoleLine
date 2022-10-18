455051e00ccc0efcb2b9f5eb6c0b19134123f63b
mintUI9976 <niklas.griese4@gmail.com>
2022-10-18 02:31:33 +0200

    update CommandArguments.java

--- src/main/java/de/mint/consoleline/commands/handler/CommandArguments.java
+++ src/main/java/de/mint/consoleline/commands/handler/CommandArguments.java
@@ -3,9 +3,21 @@ package de.mint.consoleline.commands.handler;
 import org.jetbrains.annotations.NotNull;
 
 public interface CommandArguments {
-  /** This function is called when the user types a command. */
+  /**
+   * Returns the name of the command.
+   *
+   * @return The name of the command.
+   */
   @NotNull
   String commandName();
 
+  /**
+   * "This function is called when the user types a command in the console."
+   *
+   * <p>The first thing you'll notice is that the function is marked as `default`. This means that
+   * you don't have to implement it if you don't want to
+   *
+   * @param arguments The arguments passed to the command.
+   */
   default void onCommand(final String[] arguments) {}
 }
