95ffa991eccfc4f85f5f96f5850462d04bb146cd
mintUI9976 <niklas.griese4@gmail.com>
2025-11-18 01:11:54 +0100

    remove all

--- src/main/java/de/mint/consoleline/folder/FileManager.java
+++
@@ -1,5 +0,0 @@
-package de.mint.consoleline.folder;
-
-public interface FileManager {
-    boolean createFile(String path);
-}
--- src/main/java/de/mint/consoleline/folder/FolderManager.java
+++
@@ -1,8 +0,0 @@
-package de.mint.consoleline.folder;
-
-public interface FolderManager {
-
-    boolean createFolder(String path);
-    boolean deleteFolder(String path);
-    boolean deleteFolderRecursive(String path);
-}
